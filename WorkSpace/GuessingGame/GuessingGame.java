15ed4f6d93c572729326b927d55d822f75287e77
Vishwas verma <codewithvishwas@gmail.com>
2024-09-10 19:57:10 +0530

    Altered basic Logic

--- WorkSpace/GuessingGame/GuessingGame.java
+++ WorkSpace/GuessingGame/GuessingGame.java
@@ -10,24 +10,47 @@ public class GuessingGame {
         System.out.println("Enter a Max number within what you wish to guess : ");
         int range = sc.nextInt();
         int randomNumber = random.nextInt(range) + 1;
+        
+        System.out.println("Please select the difficulty level:");
+        System.out.println("1. Easy (10 chances)");
+        System.out.println("2. Medium (5 chances)");
+        System.out.println("3. Hard (3 chances)");
+        System.out.print("Choose Your Difficulty Level: ");
+        int difficulty = sc.nextInt();
+        int difficultyAttempt=0;
+        switch(difficulty){
+            case 1:
+            difficultyAttempt = 10;
+            break;
+            case 2:
+            difficultyAttempt = 5;
+            break;
+            case 3:
+            difficultyAttempt = 4;
+            break;
+            default:
+            System.out.println("Enter a Valid difficulty level , By default , i am setting it to Easy level");
+            difficultyAttempt = 10;
+            break;
+        };
 
         int guess = 0;
         int attempts = 0;
 
-        while (guess != randomNumber) {
-            System.out.print("Enter your guess (between 1 and "+range+") : ");
-            guess = sc.nextInt();
-            attempts++;
+            while (attempts<=difficultyAttempt && guess != randomNumber) {
+                System.out.print("Enter your guess (between 1 and "+range+") : ");
+                guess = sc.nextInt();
+                attempts++;
 
-            if (randomNumber - guess == 0) {
-                System.out.println("Congratulations! You've guessed the number.");
-                System.out.println("It took you " + attempts + " attempts.");
-            } else if (randomNumber - guess < 0) {
-                System.out.println("Too high! Try again.");
-            } else if (randomNumber - guess > 0) {
-                System.out.println("Too low! Try again.");
+                if (randomNumber - guess == 0) {
+                    System.out.println("Congratulations! You've guessed the number.");
+                    System.out.println("It took you " + attempts + " attempts.");
+                } else if (randomNumber - guess < 0) {
+                    System.out.println("Too high! Try again.");
+                } else if (randomNumber - guess > 0) {
+                    System.out.println("Too low! Try again.");
+                }
             }
-        }
 
         sc.close();
     }
